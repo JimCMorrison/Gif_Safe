3fb94e4e0611247c3aa0527902f2d69632d0dae2
JimCMorrison <jmorrison@lightspeedsystems.com>
2015-12-03 21:48:30 -0800

    Added Gif Views

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,14 +1,23 @@
 class GifsController < ApplicationController
+  before_action :find_gif, only: [:show, :edit, :update, :destroy]
   def index
+    @gifs = Gif.all.order("created_at DESC")
   end
 
   def show
   end
 
   def new
+    @gif = Gif.new
   end
 
   def create
+    @gif = Gif.new(gif_params)
+    if @gif.save
+      redirect_to @gif
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -23,8 +32,10 @@ class GifsController < ApplicationController
   private
 
   def find_gif
+    @gif = Gif.find(params[:id])
   end
 
   def gif_params
+    params.require(:gif).permit(:tag, :url, :description)
   end
 end
