e0e0534a481dff27b5388346ce3c07e153f267f9
JimCMorrison <jmorrison@lightspeedsystems.com>
2015-12-05 17:02:26 -0800

    Add ability to change root if user_signed_in?

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
-
   get 'home/privacy'
+  resources :gifs
+
+  authenticated :user do
+    root'gifs#index', as: "authenticated_root"
+  end
 
   root 'home#index'
 
-  resources :gifs
 end
